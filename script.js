ea38e82ec8d295c8d9890258ac62610468afbbc1
Dosapati manish <51026237+dosapatimanish@users.noreply.github.com>
2024-08-07 22:59:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var initialPath=`M 200 100 Q 500 100 1200 100`
+
+var finalPath=`M 200 100 Q 500 100 1200 100`
+
+var string=document.querySelector("#string")
+
+
+string.addEventListener("mousemove",function(dets){
+    path=`M 200 100 Q ${dets.x} ${dets.y} 1200 100`;
+    gsap.to("svg path",{
+        attr:{d:path},
+        duration:0.3,
+        ease:"power3.out"
+    })
+})
+
+string.addEventListener("mouseleave",function(){
+    gsap.to("svg path",{
+        attr:{d:finalPath},
+        duration:1.5,
+        ease:"elastic.out(1,0.2)"
+    })
+})
